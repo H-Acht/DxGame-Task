2bfe25365a6410d3d032e61dc485c7a432fa3d2f
橋垣 冴祐 <fko2247061@stu.o-hara.ac.jp>
2022-10-13 15:33:22 +0900

    ゲーム名変更

--- game.h
+++ game.h
@@ -5,7 +5,7 @@ namespace Game
     //ウィンドウモード設定
     constexpr bool kWindowMode = true;
     //ウィンドウ名設定
-    const char* const kTitleText = "ゲーム名";
+    const char* const kTitleText = "R E N D A";
     //ウィンドウサイズ
     constexpr int kScreenWidth = 640;
     constexpr int kScreenHeight = 480;
