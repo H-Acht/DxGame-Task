2e2e938575708c4745809c3d55fd95e08be5b4c3
橋垣 冴祐 <fko2247061@stu.o-hara.ac.jp>
2022-10-14 15:38:46 +0900

    文字位置の修正

--- SceneClear3.cpp
+++ SceneClear3.cpp
@@ -13,7 +13,7 @@ SceneBase* SceneClear3::update()
 {
 	int padState = GetJoypadInputState(DX_INPUT_KEY_PAD1);
 
-	if (padState & PAD_INPUT_3)
+	if (padState & PAD_INPUT_4)
 	{
 		return (new SceneTitle);
 	}
@@ -25,5 +25,6 @@ void SceneClear3::draw()
 {
 	DrawString(m_textPosX + 75, m_textPosY - 100, "Lv.3 CLEAR!!", GetColor(255, 255, 255));
 	DrawString(m_textPosX - 110, m_textPosY, "Pless C Key or X to Back to Title", GetColor(255, 255, 255));
+	DrawString(m_textPosX + 40, m_textPosY + 100, "DO NOT PLESS [Y]", GetColor(255, 0, 0));
 
 }
