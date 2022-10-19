64c8d9c94bd3e29a20af0c55e914dd3c2e70cfa5
橋垣 冴祐 <fko2247061@stu.o-hara.ac.jp>
2022-10-19 12:30:35 +0900

    警告音の追加、ERRORの文字が流れる

--- SceneError.cpp
+++ SceneError.cpp
@@ -3,7 +3,9 @@
 #include "DxLib.h"
 void SceneError::init()
 {
-	m_textPosX = -30;
+	StopMusic();
+	PlayMusic("BGM/ErrorSiren.mp3", DX_PLAYTYPE_BACK);
+	m_textPosX = -800;
 	m_textPosY = 120;
 
 	m_countTimer = 0;
@@ -12,7 +14,7 @@ void SceneError::init()
 SceneBase* SceneError::update()
 {
 	m_countTimer++;
-	if (m_countTimer >= 100)
+	if (m_countTimer >= 250)
 	{
 		return(new SceneErrorTitle);
 	}
@@ -22,7 +24,8 @@ SceneBase* SceneError::update()
 
 void SceneError::draw()
 {
-	DrawString(m_textPosX, m_textPosY, "ERROR!!error!!ERROR!!error!!ERROR!!error!!ERROR!!error!!ERROR!!error!!ERROR!!error!!ERROR!!error!!ERROR!!", GetColor(255, 0, 0));
-	DrawString(m_textPosX + 195, m_textPosY + 200, "タイトルに戻ります", GetColor(255, 255, 255));
-
+	SetFontSize(64);
+	DrawString(m_textPosX +=3.0f, m_textPosY, "ERROR!!error!!ERROR!!error!!ERROR!!error!!ERROR!!error!!ERROR!!error!!ERROR!!error!!ERROR!!error!", GetColor(255, 0, 0));
+	SetFontSize(32);
+	DrawString(165, m_textPosY + 200, "タイトルに戻ります", GetColor(255, 255, 255));
 }
